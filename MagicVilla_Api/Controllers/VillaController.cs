cdf26edae1840c17d261a955ac6d36c69c03b0f8
rubenzOnet <rgarrido@officenet.com.mx>
2023-09-27 22:18:51 -0600

    Lee lista de villas y villa por id

--- MagicVilla_Api/Controllers/VillaController.cs
+++ MagicVilla_Api/Controllers/VillaController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MagicVilla_Api.Datos;
+using MagicVilla_Api.Models;
+using MagicVilla_Api.Models.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_Api.Controllers
@@ -7,6 +10,22 @@ namespace MagicVilla_Api.Controllers
     [ApiController]
     public class VillaController : ControllerBase
     {
+
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("id:int")]
+        public VillaDTO GetVilla(int id) 
+        {
+            return VillaStore.villaList.FirstOrDefault(w => w.Id == id);
+        }
+
+
+
+
 
     }
 }
---
+++ MagicVilla_Api/Datos/VillaStore.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_Api.Models.DTO;
+
+namespace MagicVilla_Api.Datos
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+        {
+            new VillaDTO {Id = 1, Nombre = "Vista a la piscina"},
+            new VillaDTO {Id = 2, Nombre = "Vista a la playa"}
+
+        };
+    }
+}
